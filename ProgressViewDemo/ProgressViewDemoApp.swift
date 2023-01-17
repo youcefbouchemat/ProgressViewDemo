bfc0eed32fabdb827e817b45911cad886467adba
apple <apple@MacBook-Pro.local>
2023-01-17 19:12:12 +0100

    initial commit

---
+++ ProgressViewDemo/ProgressViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressViewDemoApp.swift
+//  ProgressViewDemo
+//
+//  Created by apple on 17/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct ProgressViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
