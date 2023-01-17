b579750b2e4b23b2bd38bf30eeb4c2534b88a697
apple <apple@MacBook-Pro.local>
2023-01-17 19:28:47 +0100

    add slider with a LinearProgressViewStyle progress bar

--- ProgressViewDemo/ContentView.swift
+++ ProgressViewDemo/ContentView.swift
@@ -8,12 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var progress: Double = 0;
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 50) {
+            ProgressView("Task Progress",value: progress,total: 100)
+                .progressViewStyle(LinearProgressViewStyle(tint: progress < 30 ?
+                                                           Color.red : progress < 60 ?
+                                                           Color.blue : Color.green))
+                .font(.headline)
+            
+            Slider(value: $progress, in: 1...100,step: 1)
         }
         .padding()
     }
